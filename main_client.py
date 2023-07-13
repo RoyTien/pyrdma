b9ad36cdec6a8a33b91bb54343c13bd9e8a8a458
RoyTien <tiany10@chinatelecom.cn>
2023-07-13 09:55:34 +0800

    add argparse to specific device

--- main_client.py
+++ main_client.py
@@ -1,15 +1,22 @@
+#!/usr/bin/python3
 # main
+import argparse
 from src.rdma.socket.rdma_socket_client import RdmaSocketClient
 from src.config.config import ADDR, PORT_STR
 from src.socket.client import SocketClient
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A Python Test Program for RDMA.")
+    parser.add_argument('-d', '--ib_dev', required=True,
+                        help='RDMA device to run the tests on.')
+    args = parser.parse_args()
+    args_vars = vars(args)
     # c = RdmaClient(ADDR, PORT_STR)
     # c.request()
     # c.close()
-    c = SocketClient()
+    c = SocketClient(name=args_vars.get('ib_dev'))
     # c.request()
-    c.push_file("./test/push/src/50M.file")
+    c.push_file("/tmp/Data16g.txt")
     # c.pull_file("./test/pull/des/50M.file")
     # c = RdmaSocketClient(ADDR, PORT_STR)
     # c.request()
--- main_server.py
+++ main_server.py
@@ -1,13 +1,20 @@
+#!/usr/bin/python3
 # main
+import argparse
 from src.rdma.socket.rdma_socket_server import RdmaSocketServer
 from src.config.config import ADDR, PORT_STR, OPTIONS
 from src.socket.server import SocketServer
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A Python Test Program for RDMA.")
+    parser.add_argument('-d', '--ib_dev', required=True,
+                        help='RDMA device to run the tests on.')
+    args = parser.parse_args()
+    args_vars = vars(args)
     # s = RdmaServer(ADDR, PORT_STR, OPTIONS)
     # s.run()
     # s.close()
-    s = SocketServer()
+    s = SocketServer(name=args_vars.get('ib_dev'))
     s.serve()
     # s = RdmaSocketServer(ADDR, PORT_STR, OPTIONS)
     # s.serve()
