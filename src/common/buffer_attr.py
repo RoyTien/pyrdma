2c301418f5e834e0609cb5ba4925963b97ec5b65
RoyTien <tiany10@chinatelecom.cn>
2023-07-13 13:59:59 +0800

    Add print info (the meaning of qp_num)

--- src/common/buffer_attr.py
+++ src/common/buffer_attr.py
@@ -16,8 +16,9 @@ class BufferAttr:
         self.qp_num = qp_num
 
     def __str__(self) -> str:
-        return "buffer_addr: %s;\nbuffer_len: %s;\nlkey: %s;\nrkey: %s;\ngid: %s;\nqp_num: %s;\n" % \
-               (self.addr, self.length, self.local_stag, self.remote_stag, self.gid, self.qp_num)
+        return "buffer_addr: %s;\nbuffer_len: %s;\nlkey: %s;\nrkey: %s;\ngid: %s;\nqp_num: %s;\n%s\n" % \
+               (self.addr, self.length, self.local_stag, self.remote_stag, self.gid, self.qp_num, 
+                "\"QP Number 简称为QPN，就是每个QP的编号。IB协议中规定用24个bit来表示QPN，即每个节点最大可以同时使用个QP。每个节点都各自维护着QPN的集合，相互之间是独立的，即不同的节点上可以存在编号相同的QP。\"")
 
     def __len__(self) -> int:
         return len(serialize(self))
