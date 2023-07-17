126f5ad8a272feeb59049eabf8dc992136f3790d
RoyTien <tiany10@chinatelecom.cn>
2023-07-17 14:08:24 +0800

    add file arg and gid explain

--- main_client.py
+++ main_client.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A Python Test Program for RDMA.")
     parser.add_argument('-d', '--ib_dev', required=True,
                         help='RDMA device to run the tests on.')
+    parser.add_argument('-f', '--file', type=str, 
+                        help='The path of file to be transfered.')
     args = parser.parse_args()
     args_vars = vars(args)
     # c = RdmaClient(ADDR, PORT_STR)
@@ -16,7 +18,8 @@ if __name__ == "__main__":
     # c.close()
     c = SocketClient(name=args_vars.get('ib_dev'))
     # c.request()
-    c.push_file("/tmp/Data16g.txt")
+    file_path = args_vars.get('file') if args_vars.get('file') else "./test/push/src/test.file"
+    c.push_file(file_path)
     # c.pull_file("./test/pull/des/50M.file")
     # c = RdmaSocketClient(ADDR, PORT_STR)
     # c.request()
--- src/config/config.py
+++ src/config/config.py
@@ -3,14 +3,14 @@ import pyverbs.enums as e
 ADDR = "192.168.236.129"
 PORT_INT = 50008
 PORT_STR = "50008"
-NAME = "rxe_0"
+NAME = "mlx5_3"
 TIMEOUT_IN_MS = 500
 BUFFER_SIZE = 1024
 FILE_SIZE = 10 * BUFFER_SIZE * BUFFER_SIZE  # 10MB
 
 OPTIONS = {
     "qp_init": {
-        "qp_type": e.IBV_QPT_RC,
+        "qp_type": e.IBV_QPT_RC,  # https://stackoverflow.com/questions/35620376/to-create-multiple-queue-pairs-in-rdma
         "max_send_wr": 4,
         "max_recv_wr": 4,
         "max_send_sge": 2,
@@ -21,6 +21,6 @@ OPTIONS = {
     },
     "gid_init": {
         "port_num": 1,
-        "gid_index": 1,  # 3 mean to use the RoCE v2 interface
+        "gid_index": 3,  # 3 mean to use the RoCE v2 interface `$ show_gids`
     }
 }
